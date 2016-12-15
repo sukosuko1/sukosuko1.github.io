3ead57be4e42624d3ac0852a1325250ceef5333d
sukosuko1 <sukosuko1@gmail.com>
2016-12-16 00:34:58 +0800

    dec2016b

--- particle.js
+++ particle.js
@@ -217,7 +217,7 @@ Particle.prototype.update = function() {
     this.x += this.vx
     this.y += this.vy ;
     this.vx += world.ax * 0.2;
-    this.vy += world.gravity+ world.ay * 0.2;
+    this.vy += world.gravity+ world.ay * -0.2;
     
     if (this.y > canvas.height) if (this.vy > 0) this.vy = -1 * this.vy * Math.random() * 0.5;
     
