0ae58b061db65c01d9336c65bb9232fc6dd45559
sukosuko1 <sukosuko1@gmail.com>
2017-03-24 21:52:10 +0800

    new1

--- particlephoto2.js
+++ particlephoto2.js
@@ -309,6 +309,12 @@ var texty = 80;
         viewportmeta.content = "user-scalable=0";
     }
 
+
+var $viewportMeta = $('meta[name="viewport"]');
+$('input, select, textarea').bind('focus blur', function(event) {
+$viewportMeta.attr('content', 'width=device-width,initial-scale=1,maximum-scale=' +        (event.type == 'blur' ? 10 : 1));
+});
+
 $("#text1").text(window.width);
 
 $(document).ready(function() {
