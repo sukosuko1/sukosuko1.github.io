8ba13612aeac471761deaf54de3b844ec139c031
Scott Christensen <scottchristensen@Scotts-MacBook-Air.local>
2017-11-28 22:08:28 +0800

    speech bubble

--- sukoCanvas.js
+++ sukoCanvas.js
@@ -35,6 +35,9 @@ var SpeechBubble = {
 		obj.dx = 0;
 		return obj;
 	},
+	calcY: function() {
+		return this.y;
+	},
 	drawBubble: function drawBubble(ctx, x, y, w, h, radius) {
 		var r = x + w;
   		var b = y + h;
@@ -76,15 +79,18 @@ var SpeechBubble = {
 		ctx.fill();
 	},
 	action : function(elapsed) {
-		if (this.textgen) {
-			var r = this.textgen.action(elapsed);
-        	if (r == 2) {
-            	this.offsetx = Math.random() * 20;
-            	this.offsety = Math.random() * 20;
+		if (this.myObjs) {
+			for (var i = 0; i < this.myObjs.length; i++) {
+				this.myObjs[i].action();
 			}
 		}
 	},
 	render : function(ctx,dispX,dispY) {
+		if (this.myObjs) {
+			for (var i = 0; i < this.myObjs.length; i++) {
+				this.myObjs[i].render(ctx,dispX,dispY);
+			}
+		}
         if (this.textgen.text != "") {
 
         var lineNum = wrapText(ctx, this.textgen.text, 
@@ -99,6 +105,9 @@ var SpeechBubble = {
 //		ctx.fillRect(x+0,y+0,200,50);		
 		this.drawBubble(ctx,dispX+this.offsetx,dispY+this.offsety,180-this.offsetx, 20+ 20 * lineNum,10);
 
+		this.width = 180;
+		this.height = 20 + 20 * lineNum;
+			
 		ctx.shadowBlur = 0;
 		ctx.fillStyle = "black";
         ctx.font = "20px Helvetica"
@@ -107,5 +116,62 @@ var SpeechBubble = {
         wrapText(ctx, this.textgen.text, 
 				 dispX + 8+this.offsetx, dispY + 25+this.offsety, 165-this.offsetx, 20);            
         }
+	} 
+}
+
+var Particle = {
+	create : function (parent,x,y,dx,dy) {
+		var obj = Object.create(this);
+		obj.parent = parent;
+		obj.lifetime = 50;
+		obj.x = x;
+		obj.dx = dx;
+		obj.y = y;
+		obj.dy = dy;
+		return obj;
+	},
+	action : function(elapsed) {
+		this.lifetime--;
+		this.dy += 0.1;
+		this.y += this.dy;
+		this.x += this.dx;
+	},
+	render : function(ctx,dispX,dispY) {
+		ctx.globalAlpha = this.lifetime / 30;
+		ctx.fillStyle = 'rgb(' + 255 * this.lifetime / 30 + ",55,55)";
+		ctx.fillRect(this.x, this.y, 10, 10); 			
+		ctx.globalAlpha = 1;
+	} 
+}
+
+var BackgroundSparkle = {
+	create : function (parent) {
+		var obj = Object.create(this);
+		obj.parent = parent;
+		obj.state = 0;
+		obj.state2 = 0;
+		obj.myObjs = [];
+		return obj;
+	},
+	action : function(elapsed) {
+		this.state = Math.random()*5;
+		this.state2 = Math.random()*5;
+		
+		this.myObjs.push(Particle.create(this, this.parent.x + Math.random()*this.parent.width, this.parent.y, 0,0));
+		for (var i = 0; i < this.myObjs.length; i++) {
+			var obj = this.myObjs[i];
+			obj.action(elapsed);
+			if (obj.lifetime <= 0) this.myObjs.splice(i,i);
+		}
+	},
+	render : function(ctx,dispX,dispY) {
+//		for (i = 0; i < 50; i++) {
+//			ctx.fillStyle = "gold";
+//			ctx.fillRect(dispX+(-0.2+1.4*Math.random())*this.parent.width, 			
+//						 dispY+(-0.2+1.4*Math.random())*this.parent.height,20,20);		
+//		}
+		for (var i=0; i< this.myObjs.length;i++) {
+			this.myObjs[i].render(ctx,dispX,dispY);
+		}
 	} 
 }
