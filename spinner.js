818f5660e968e4083f3da7ccf42fc54a95b3d086
Scott Christensen <scottchristensen@Scotts-MacBook-Air.local>
2018-01-04 00:21:30 +0800

    spiinner2

--- spinner.js
+++ spinner.js
@@ -11,17 +11,14 @@ function Spinner() {
 		  this.render = function(ctx) {
 			  ctx.lineWidth = 10;
 			  ctx.strokeStyle = "#aa00aa"
-			  ctx.beginPath();
-			  ctx.arc(0,0,250, this.timer, this.timer+5.0);
-			  ctx.stroke();
 			  
 			  ctx.save();
 			  ctx.globalAlpha = 0.7;
-			  ctx.translate(ctx.canvas.width * 0.9, ctx.canvas.height * 0.9); 
+//			  ctx.translate(ctx.canvas.width * 0.9, ctx.canvas.height * 0.9); 
 			  ctx.rotate(this.timer);
 //			  ctx.fillRect(-200,-10,400,20);
-			  ctx.fillRect(-100,-100,200,30);
-			  ctx.fillRect(-100,70,200,30);
+			  ctx.fillRect(-10,-100,20,60);
+			  ctx.fillRect(-10,40,20,60);
 			  ctx.beginPath();
 			  ctx.arc(0,0,50, this.timer, this.timer+5.0);
 			  ctx.stroke();
